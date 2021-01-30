d8a9c8dc693ce04655985255cfd2bf5940ae1f29
moonken <u851128@>
2021-01-30 19:18:58 +0800

    添加成员管理

--- packages/components/member-list.vue
+++ packages/components/member-list.vue
@@ -84,7 +84,7 @@ export default {
               value: 'id',
             },
             filterable: false,
-            dicUrl:  process.env.VUE_APP_API_URL + 'users',
+            dicUrl:  process.env.VUE_APP_API_URL + '/users',
             dicMethod: 'get',
           },
           {
